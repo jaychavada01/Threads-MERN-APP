73ec57f050bfd03c58d95a51a201937bc9c05ec9
jaychavada01 <chavdajay2707@gmail.com>
2024-04-05 11:09:47 +0530

    user and post model add

--- backend/db/database.js
+++ backend/db/database.js
@@ -1,18 +1,14 @@
 import mongoose from "mongoose";
 
 const database = async () => {
-  	try {
-      const conn = await mongoose.connect(process.env.MONGODB_URL, {
-        // To avoid warnings in the console
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      });
+  try {
+    const conn = await mongoose.connect(process.env.MONGODB_URL);
 
-      console.log(`🍃 MongoDB Connected: ${conn.connection.host}`);
-    } catch (error) {
-      console.error(`Error: ${error.message}`);
-      process.exit(1);
-    }
+    console.log(`🍃 MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
+  }
 };
 
 export default database;
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import database from "./db/database.js";
 
@@ -10,6 +11,12 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+// Middlewares
+app.use(express.json()); // To parse JSON data in the req.body
+app.use(express.urlencoded({ extended: true })); // To parse form data in the req.body
+app.use(cookieParser());
+
+
 app.listen(PORT, () => {
-  console.log(`🌐 Server started at http://localhost:${PORT}`);
+  console.log(`🌐 Server started at port ${PORT}`);
 });
